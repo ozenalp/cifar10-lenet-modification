518c8187f0b63f1f4d965d65884fb7a208306b74
Alparslan Özen <ozen_alparslan@outlook.com>
2023-04-30 22:25:26 +0300

    Create train.py
    This commit introduces train.py, which includes two functions: train_model and evaluate. These functions are used for training and evaluating machine learning models

---
+++ scripts/train.py
@@ -0,0 +1,59 @@
+from utils import set_seed, calculate_accuracy
+from tqdm import tqdm
+import torch
+
+SEED = 1773
+set_seed(SEED)
+
+def train_model(model, iterator, optimizer, criterion, device):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for (x, y) in tqdm(iterator, desc="Training", leave=False):
+        x = x.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+
+        y_pred, _ = model(x)
+
+        loss = criterion(y_pred, y)
+
+        acc = calculate_accuracy(y_pred, y)
+
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion, device):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for (x, y) in tqdm(iterator, desc="Evaluating", leave=False):
+
+            x = x.to(device)
+            y = y.to(device)
+
+            y_pred, _ = model(x)
+
+            loss = criterion(y_pred, y)
+
+            acc = calculate_accuracy(y_pred, y)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
